c5ba081ff56453c6ffc34e984a764ddae5a47b33
andylitvinov-design <andy.litvinov@gmail.com>
2026-05-30 08:23:07 +0200

    Import template services styles in master panel

--- src/components/MasterTemplateServicesPanel.jsx
+++ src/components/MasterTemplateServicesPanel.jsx
@@ -14,6 +14,7 @@ import {
   updateServiceOrder
 } from "../lib/profileServicesClient.js";
 import { uploadProfileMedia } from "../lib/profileMediaClient.js";
+import "../templateServices.css";
 
 const CURRENCY_OPTIONS = ["EUR", "USD", "CAD", "UAH"];
 
