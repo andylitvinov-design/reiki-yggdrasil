5d8846b5744109fd2e504c7b84f117927879dc5e
andylitvinov-design <andy.litvinov@gmail.com>
2026-05-31 12:28:36 +0200

    Revert forced Google OAuth token response type

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -168,9 +168,6 @@ export function signInWithGoogle(redirectPath = "/profile") {
 
   authorizeUrl.searchParams.set("provider", "google");
   authorizeUrl.searchParams.set("redirect_to", redirectTo);
-  // This app stores Supabase sessions from the URL hash. Request the implicit token flow
-  // explicitly so Google OAuth returns #access_token instead of a PKCE ?code callback.
-  authorizeUrl.searchParams.set("response_type", "token");
 
   window.location.assign(authorizeUrl.toString());
 }
