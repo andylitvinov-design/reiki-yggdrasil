fc7a9bd83a92f6ca53ea69f7ec4b296e2ba3ac3e
andylitvinov-design <andy.litvinov@gmail.com>
2026-06-02 22:08:19 +0200

    feat: add first photo mandala template pilot

---
+++ src/data/placePowerMandalaTemplates.js
@@ -0,0 +1,21 @@
+export const placePowerMandalaTemplates = [
+  {
+    id: "stone-mosaic-01",
+    label: "1",
+    title: "Каменная мандала 1",
+    src: "/mandala-templates/place-power/stone-mosaic-01.svg",
+    coverTone: "template-stone-mosaic-01",
+    format: "mandala",
+    zones: [
+      { id: "client-1", label: "Центр", className: "center" },
+      { id: "client-2", label: "Верхний ромб", className: "top" },
+      { id: "client-3", label: "Правый ромб", className: "right" },
+      { id: "client-4", label: "Нижний ромб", className: "bottom" },
+      { id: "client-5", label: "Левый ромб", className: "left" },
+      { id: "client-6", label: "Верхняя левая звезда", className: "topLeft" },
+      { id: "client-7", label: "Верхняя правая звезда", className: "topRight" },
+      { id: "client-8", label: "Нижняя левая звезда", className: "bottomLeft" },
+      { id: "client-9", label: "Нижняя правая звезда", className: "bottomRight" }
+    ]
+  }
+];
